97f9623b48dc573c3d545890339dda68d6da0be2
prince-nkuru <nkuruprinces@gmail.com>
2024-07-08 21:35:33 +0530

    modify navbar file

--- src/navbar.js
+++ src/navbar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (  
    <nav className="navbar">
     <h1>new navbar</h1>
-    <div>
+    <div className="link">
       <a href="/">home</a>
       <a href="/create">blog</a>
     </div>
