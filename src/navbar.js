4907bc173d39888741b7a4686d2cd79e2a8831d4
prince-nkuru <nkuruprinces@gmail.com>
2024-07-08 21:40:30 +0530

    add inline styling

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,10 @@ const Navbar = () => {
     <h1>new navbar</h1>
     <div className="link">
       <a href="/">home</a>
-      <a href="/create">blog</a>
+      <a href="/create" style={{
+        color: 'white',
+        backgroundColor: '#f1356d'
+      }} >blog</a>   {/* you may also use inline styling */}
     </div>
    </nav>
   );
