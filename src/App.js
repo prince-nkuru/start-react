fd6cf4c6ffbc41050b2c13bdeb81afbce57cca74
prince-nkuru <nkuruprinces@gmail.com>
2024-07-09 20:27:42 +0530

    removed duplicate codes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-const subject = 'react'
-function App() {
-  return (
-    <div className="App">
-      <div className='content'>
-        <h1>app component {subject}</h1>
-      </div>
-    </div>
-  );
-}
 
-export default App;
+// function Square({ value }) {
+//   return <button className="square">{value}</button>;
+// }
+
+// export default function Board() {
+//   return (
+//     <>
+//       <div className="board-row">
+//         <Square value="1" />
+//         <Square value="2" />
+//         <Square value="3" />
+//       </div>
+//       <div className="board-row">
+//         <Square value="4" />
+//         <Square value="5" />
+//         <Square value="6" />
+//       </div>
+//       <div className="board-row">
+//         <Square value="7" />
+//         <Square value="8" />
+//         <Square value="9" />
+//       </div>
+//     </>
+//   );
+// }
+
