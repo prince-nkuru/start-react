d368262adb88ad2dd0a0db65c25ba055110dacb0
prince-nkuru <nkuruprinces@gmail.com>
2024-07-09 20:56:21 +0530

    add new file navbar

---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+const navBar = () => {
+  return (  
+   <nav className="navbar">
+    <h1>new navbar</h1>
+    <div>
+      <a href="/">home</a>
+      <a href="/create">blog</a>
+    </div>
+   </nav>
+  );
+}
+ 
+export default navBar;
