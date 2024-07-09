55d6e118e23f2538598f90b1000a595be8aa81de
prince-nkuru <nkuruprinces@gmail.com>
2024-07-09 20:31:09 +0530

    add className add

--- src/App.js
+++ src/App.js
@@ -26,3 +26,13 @@ import './App.css';
 //   );
 // }
 
+function App (){
+  return (
+    <div className='App'>
+      <div className='content'>
+        <h1>app content</h1>
+      </div>
+    </div>
+  )
+}
+export default App
