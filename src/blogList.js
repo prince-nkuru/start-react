6d10d0dbf199ed042a67f21b7b1b09f1608ec54e
prince-nkuru <nkuruprinces@gmail.com>
2024-07-10 17:36:36 +0530

    add blogList file

---
+++ src/blogList.js
@@ -0,0 +1,25 @@
+const BlogList = (props) => {
+
+  const blogs = props.blogs;
+  const titles = props.titles;
+  return ( 
+    
+    <div className="home">
+      <h1>{titles}</h1>
+    {blogs.map((blog) => (
+     
+      <div className="blog-preview" key={blog.id}>
+        
+        <h2>{blog.title}</h2>
+        <p> Written by {blog.author}</p>
+        <button onClick={() => handleDelete(blog.id)}>remove blog</button>
+      </div>
+     ))}
+    
+  </div>
+  
+  
+)}
+
+ 
+export default BlogList;
