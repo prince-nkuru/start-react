d90d62c651b1b665c2c7568631beff848c080907
prince-nkuru <nkuruprinces@gmail.com>
2024-07-10 10:28:59 +0530

    add blogs list

--- src/home.js
+++ src/home.js
@@ -1,21 +1,40 @@
 import { useState } from "react";
 
+// const Home = () => {
+
+//   let [count, setName] = useState(0);
+//   const Greeting = (name) => {
+
+//     setName(prevCount => prevCount + 1);
+//        const short = name? 'hello ' + name: 'hello there'
+//        console.log(short, count++);
+//   }
+//   return (  
+//     <div className="home">
+//       <h2>homepage</h2>
+//       <p>{count}</p>
+//       <button onClick={() =>{
+//       Greeting('prince');
+//       }}>click me</button>
+//     </div>
+//   );
+// }
+
+
 const Home = () => {
-
-  let [count, setName] = useState(0);
-  const Greeting = (name) => {
-
-    setName(prevCount => prevCount + 1);
-       const short = name? 'hello ' + name: 'hello there'
-       console.log(short, count++);
-  }
+ const [blogs, setBlogs] = useState([
+  {title: 'my blog', author: 'prince', body : 'lorem ipsum', id : 1},
+  {title: 'news paper', author: 'nkuruniza', body : 'lorem ipsum', id : 2},
+  {title: 'my blog', author: 'prince', body : 'lorem ipsum', id : 3},
+ ])
   return (  
     <div className="home">
-      <h2>homepage</h2>
-      <p>{count}</p>
-      <button onClick={() =>{
-      Greeting('prince');
-      }}>click me</button>
+      {blogs.map(blog => {
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p> Written by {blog.author}</p>
+        </div>
+      })}
     </div>
   );
 }
