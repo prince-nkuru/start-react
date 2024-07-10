525f67f5a403c3ab8354c8196b8730e20e51a0a1
prince-nkuru <nkuruprinces@gmail.com>
2024-07-10 09:05:46 +0530

    add event handling

--- src/home.js
+++ src/home.js
@@ -1,7 +1,14 @@
 const Home = () => {
+  const Greeting = (name) => {
+       const short = name? 'hello ' + name: 'hello there'
+       console.log(short);
+  }
   return (  
     <div className="home">
       <h2>homepage</h2>
+      <button onClick={() =>{
+      Greeting();
+      }}>click me</button>
     </div>
   );
 }
