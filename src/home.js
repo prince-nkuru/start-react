2e21954c516e5dc71eb7d717553d402c0d1029d7
prince-nkuru <nkuruprinces@gmail.com>
2024-07-10 09:24:08 +0530

    add useState hook

--- src/home.js
+++ src/home.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
+
+  const [name, setName] = useState('prince');
   const Greeting = (name, e) => {
+
+    setName('nkurunziza');
        const short = name? 'hello ' + name: 'hello there'
        console.log(short, e.target);
   }
   return (  
     <div className="home">
       <h2>homepage</h2>
+      <p>{name}</p>
       <button onClick={(e) =>{
       Greeting('prince', e);
       }}>click me</button>
