f1084a6140f9fee65f45538ffffa5dc01f84bee6
prince-nkuru <nkuruprinces@gmail.com>
2024-07-12 15:30:55 +0530

    add object

--- src/object.js
+++ src/object.js
@@ -7,14 +7,23 @@ function MyObject (){
                                  })
 
    const handleYear = (event) => {
-    setCar({...car, year:2025})
+    setCar(c => ({...c, year:event.target.value}))
    } 
+
+   const handleMake = (event) => {
+    setCar(c => ({...c, make:event.target.value}))
+   }
    
+   const handleModel = (event) => {
+    setCar(c => ({...c,model:event.target.value}))
+   }
    
    return (
     <div>
       <p>my favourite car is {car.year} {car.make} {car.model}</p>
-      <input type="number" onChange={handleYear}/>
+      <input type="number" value={car.year} onChange={handleYear}/><br/>
+      <input type="text" value={car.make} onChange={handleMake}/><br/>
+      <input type="text" value={car.model} onChange={handleModel}/>
     </div>
    )
 }
