9ae642bf65780f24e5247470f4f17098936c42ac
prince-nkuru <nkuruprinces@gmail.com>
2024-07-14 11:56:58 +0530

    import todo list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Navbar from './navbar';
-import Home from './home';
-import Naming from './setName'
-import Counter from './counter'
-import MyComponent from './onChange';
-import ColorPicker from './colorPicker';
-import  MyObject from './object';
-import Foods from './array';
-import UpdateArray from './updateArray';
+// import Navbar from './navbar';
+// import Home from './home';
+// import Naming from './setName'
+// import Counter from './counter'
+// import MyComponent from './onChange';
+// import ColorPicker from './colorPicker';
+// import  MyObject from './object';
+// import Foods from './array';
+// import UpdateArray from './updateArray';
 
 // function Square({ value }) {
 //   return <button className="square">{value}</button>;
@@ -34,31 +34,48 @@ import UpdateArray from './updateArray';
 //   );
 // }
 
-function App (){
-  // const blog = '';
-  // const like = 80;
-  return (
-    <div className='App'>
-      <Navbar/>
-      <div className='content'>
-        <Home/>
-        {/* <h1>app content</h1> */}
-        {/* <h1>{blog? blog: 'no new blog'}</h1>
-        <p>liked {like} times</p>              {/* boolean and object are not used as variable ?? */}
-        {/* <p>{Math.floor(Math.random() * 10)}</p>  */}
+// function App (){
+//   // const blog = '';
+//   // const like = 80;
+//   return (
+//     <div className='App'>
+//       <Navbar/>
+//       <div className='content'>
+//         <Home/>
+//         {/* <h1>app content</h1> */}
+//         {/* <h1>{blog? blog: 'no new blog'}</h1>
+//         <p>liked {like} times</p>              {/* boolean and object are not used as variable ?? */}
+//         {/* <p>{Math.floor(Math.random() * 10)}</p>  */}
 
-        <Naming greet = 'is doing great'/>
-        <Counter/>
-        <MyComponent/>
-        <ColorPicker/>
-        <MyObject/>
-        <Foods/>
+//         <Naming greet = 'is doing great'/>
+//         <Counter/>
+//         <MyComponent/>
+//         <ColorPicker/>
+//         <MyObject/>
+//         <Foods/>
 
-        <UpdateArray/>
-      </div>
+//         <UpdateArray/>
+//       </div>
       
-    </div>
-  )
-}
-export default App
+//     </div>
+//   )
+// }
+// export default App
+
+   import TodoList from "./todoList" 
+
+function App (){
+  
+    return (
+      <>
+      <div>
+        <TodoList/>
+      </div>
+      </>
+    )
+  }
+  export default App
+  
+
+
 
