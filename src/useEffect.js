7a0f7dff21ea154768ae9b3510981410d0a96ce7
prince-nkuru <nkuruprinces@gmail.com>
2024-07-14 20:05:02 +0530

    add some lines of code

--- src/useEffect.js
+++ src/useEffect.js
@@ -2,15 +2,24 @@ import { useState, useEffect } from "react";
 
 function MyComp(){
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState('green')
+
+  useEffect(() => {
+    document.title = `count: ${count}`
+  }, [count])
 
   const change = () => {
     setCount(c => c + 1)
   }
+  const changeColor = () => {
+    setColor(c => c === 'green'? 'red' : 'green')
+  }
 
   return (
     <div>
-      <p>count : {count}</p>
+      <p style={{color: color}}>count : {count}</p>
       <button onClick={change}>add</button>
+      <button onClick={changeColor}>color</button>
     </div>
   )
 }
