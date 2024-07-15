cc5362f1870f5f951a8d6af3182c549a4362423b
prince-nkuru <nkuruprinces@gmail.com>
2024-07-15 21:17:08 +0530

    adding custom hook

---
+++ src/useFetch.js
@@ -0,0 +1,7 @@
+import { useState,useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = uuseState(true);
+  const [error,setError] = useState(null)
+}
