dba684b3f1b218f30587bfd43cb2b4ff5ecc63a8
prince-nkuru <nkuruprinces@gmail.com>
2024-07-15 21:27:56 +0530

    adding full hook

--- src/useFetch.js
+++ src/useFetch.js
@@ -3,5 +3,31 @@ import { useState,useEffect } from "react";
 const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = uuseState(true);
-  const [error,setError] = useState(null)
+  const [error,setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+    .then(res => {
+      if (!res.ok){
+        
+        throw Error ('could not fetch data for that resources')
+
+      }
+      return res.json()
+    
+    })
+    .then(data =>{
+      setData(data)
+      setIsPending(false)
+      setError(null)
+    })
+    .catch(err => {
+      setIsPending(false)
+      setError(err.message)
+    })
+  }, [url])
+
+  return {data, isPending,error}
 }
+
+export default useFetch;
